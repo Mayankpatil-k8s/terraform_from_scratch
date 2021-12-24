68dfd07a65ef541a3550f0207d2960f032724438
mayank2815794 <77990031+mayank2815794@users.noreply.github.com>
2021-12-25 02:01:16 +0530

    Create 2_map.tf

---
+++ 02_Terraform_Input_Variables/2_map.tf
@@ -0,0 +1,22 @@
+provider "aws" {
+   region     = "eu-central-1"
+  
+}
+resource "aws_instance" "vm_instance" {
+
+   ami           = "ami-0767046d1677be5a0"
+   instance_type =  "t2.micro"
+
+   tags = var.project_environment
+
+}
+
+
+variable "project_environment" {
+  description = "project name and environment"
+  type        = map(string)
+  default     = {
+    project     = "project-alpha",
+    environment = "dev"
+  }
+}
